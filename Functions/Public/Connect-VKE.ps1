1d7486370b0b42e15afd1a90c961dde0f2a2ddbf
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2018-11-05 22:16:59 +0100

    Update

--- Functions/Public/Connect-VKE.ps1
+++ Functions/Public/Connect-VKE.ps1
@@ -27,7 +27,7 @@ Function Connect-VKE {
  
  #>
 
-#[CmdletBinding(DefaultParametersetName="Token")][OutputType('System.Management.Automation.PSObject')]
+[CmdletBinding(DefaultParametersetName="Token")][OutputType('System.Management.Automation.PSObject')]
 
     Param (
 
@@ -46,19 +46,27 @@ Function Connect-VKE {
 
 Get-CSPAccessToken -RefreshToken $Token 
 
-$VKE_AUTH_ENDPOINT = (Invoke-WebRequest -Method GET -Uri https://$Endpoint/v1/system/auth | ConvertFrom-Json) 
-$VKE_AUTH_ENDPOINTv2 = $VKE_AUTH_ENDPOINT.endpoint
-$VKE_TOKEN_ENDPOINT = (Invoke-WebRequest -Method GET $VKE_AUTH_ENDPOINTv2/openidconnect/$OrgId/.well-known/openid-configuration | ConvertFrom-Json)                          
-$VKE_TOKEN_ENDPOINTv2 = $VKE_TOKEN_ENDPOINT.token_endpoint 
-$CSP_ACCESS_TOKEN = $env:cspAuthToken
-$VKE_ACCESS_TOKEN = (Invoke-RestMethod -ContentType "application/x-www-form-urlencoded" -Method POST -Body "grant_type=urn:vmware:grant_type:federation_token&access_token=${CSP_ACCESS_TOKEN}&scope=openid offline_access rs_admin_server at_groups id_groups rs_vmdir" -Uri $VKE_TOKEN_ENDPOINTv2)
+try {
+    $VKE_AUTH_ENDPOINT = (Invoke-WebRequest -Method GET -Uri https://$Endpoint/v1/system/auth | ConvertFrom-Json) 
+    $VKE_AUTH_ENDPOINTv2 = $VKE_AUTH_ENDPOINT.endpoint
+    $VKE_TOKEN_ENDPOINT = (Invoke-WebRequest -Method GET $VKE_AUTH_ENDPOINTv2/openidconnect/$OrgId/.well-known/openid-configuration | ConvertFrom-Json)                          
+    $VKE_TOKEN_ENDPOINTv2 = $VKE_TOKEN_ENDPOINT.token_endpoint 
+    $CSP_ACCESS_TOKEN = $env:cspAuthToken
+    $VKE_ACCESS_TOKEN = (Invoke-RestMethod -ContentType "application/x-www-form-urlencoded" -Method POST -Body "grant_type=urn:vmware:grant_type:federation_token&access_token=${CSP_ACCESS_TOKEN}&scope=openid offline_access rs_admin_server at_groups id_groups rs_vmdir" -Uri $VKE_TOKEN_ENDPOINTv2)
 
 
-    $Global:VKEConnection = [PSCustomObject] @{
-        Endpoint = $Endpoint
-        APIToken = $VKE_ACCESS_TOKEN.id
-        OrgId = $OrgId
+        $Global:VKEConnection = [PSCustomObject] @{
+            Endpoint = $Endpoint
+            APIToken = $VKE_ACCESS_TOKEN
+            OrgId = $OrgId
+        }
+
+     
     }
+    catch [Exception]{
 
-    Write-Output $VKEConnection
+        throw
+
+    }
+    Write-Output "Connected." $VKEConnection
 }
