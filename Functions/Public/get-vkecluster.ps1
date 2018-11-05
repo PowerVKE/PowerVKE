4fc9d9eeb8f2fe46ead3232bd61a95212efaf394
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2018-11-05 22:40:46 +0100

    Create get-vkecluster.ps1

---
+++ Functions/Public/get-vkecluster.ps1
@@ -0,0 +1,31 @@
+Function get-vkecluster {
+
+<#
+    .SYNOPSIS
+    Lists all VKE clusters
+    
+    .DESCRIPTION
+    List all clusters
+    
+    .INPUTS
+
+    .OUTPUTS
+ 
+    .EXAMPLE
+ 
+ #>
+
+$btoken = "Bearer " + $Global:VKEConnection.APIToken.access_token  
+
+try {
+$clusters = Invoke-WebRequest -Uri "$uri/v1/orgs/$Global:VKEConnection.OrgId/clusters" -Method GET -ContentType "application/json" -UseBasicParsing -Headers @{"Authorization"=$btoken} | ConvertFrom-json)
+invoke
+     
+    }
+    catch [Exception]{
+
+        throw
+
+    }
+    Write-Output "Connected." $VKEConnection
+}
