2140d1d9a432b81dad3e098288bd5a894414817e
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2018-11-05 21:28:50 +0100

    Update Functions/Public/Disconnect-VKE.ps1

---
+++ Functions/Public/Disconnect-VKE.ps1
@@ -0,0 +1,45 @@
+function Connect-VKE {
+
+<#
+    .SYNOPSIS
+    Connect to VMware VKE API Endpoint
+    
+    .DESCRIPTION
+    Connect to VMware VKE API Endpoint VMware Cloud Services and recieve an API token. 
+    
+    .PARAMETER Endpoint
+    VKE Endpoint to connect to.
+    
+    .PARAMETER Token
+    Refresh Token for VKE.
+
+    .PARAMETER OrgId
+    Organisation ID for the VKE instance.
+    
+    .INPUTS
+    System.String
+
+    .OUTPUTS
+
+ 
+    .EXAMPLE
+ 
+ #>
+
+[CmdletBinding(DefaultParametersetName="Token")][OutputType('System.Management.Automation.PSObject')]
+
+    Param (
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Endpoint="https://api.vke.cloud.vmware.com/",
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Token,
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$OrgId  
+    )
+}
